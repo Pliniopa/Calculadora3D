9d755c013f69c285656ce8273adc45f2cee1ab99
Pliniopa <plinio.parra520@gmail.com>
2025-10-28 13:22:59 -0500

    V1.2

--- app.py
+++ app.py
@@ -178,8 +178,8 @@ def resumen():
 
     for producto in Productos:
         # Cálculos detallados
-        costo_material = (producto[constantes.texto19] / producto[constantes.texto16]) * producto[constantes.texto14]
-        costo_laboral = (producto[constantes.texto21] / 60) * producto[constantes.texto17]
+        costo_material = (producto[constantes.texto14] / producto[constantes.texto16]) * producto[constantes.texto19]
+        costo_laboral = producto[constantes.texto17]  # Valor directo de hora laboral
         costo_luz = (producto[constantes.texto21] / 60) * producto[constantes.texto15]
         subtotal_pieza = costo_material + costo_laboral + costo_luz
         costo_pieza = round(subtotal_pieza / producto[constantes.texto27], 2)
@@ -228,7 +228,7 @@ def descargar_csv():
 
         for producto in Productos:
             # Cálculos
-            costo_material = (producto[constantes.texto19] / producto[constantes.texto16]) * producto[constantes.texto14]
+            costo_material = (producto[constantes.texto14] / producto[constantes.texto16]) * producto[constantes.texto19]
             costo_laboral = producto[constantes.texto17]  # Valor directo de hora laboral
             costo_luz = (producto[constantes.texto21] / 60) * producto[constantes.texto15]
             subtotal_pieza = costo_material + costo_laboral + costo_luz
